4561c3d98caaf1edd498c2190790a5e48d1f1ddd
James Carnegie <james.carnegie@docker.com>
2024-06-20 12:57:19 +0100

    Add support for separate attestation storage repo
    * Move mapping file types and parsing to config package
    * Change signature of Verify to take image/platform
    * Separate Attestation Resolvers to their own files (registry, layout and referrers)
    * Add support configuring referrers resolution style in mapping.yaml

--- pkg/oci/types.go
+++ pkg/oci/types.go
@@ -2,7 +2,7 @@ package oci
 
 import (
 	"fmt"
-	"log"
+	"strings"
 
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
@@ -48,12 +48,12 @@ func SubjectIndexFromPath(path string) (*SubjectIndex, error) {
 func SubjectIndexFromRemote(image string) (*SubjectIndex, error) {
 	ref, err := name.ParseReference(image)
 	if err != nil {
-		log.Fatalf("Failed to parse image name: %v", err)
+		return nil, fmt.Errorf("failed to parse image reference %s: %w", image, err)
 	}
 	// Get the authenticator from the default Docker keychain
 	auth, err := authn.DefaultKeychain.Resolve(ref.Context())
 	if err != nil {
-		log.Fatalf("Failed to get authenticator: %v", err)
+		return nil, fmt.Errorf("failed to resolve auth for image %s: %w", image, err)
 	}
 	// Pull the image from the registry
 	idx, err := remote.Index(ref, remote.WithAuth(auth))
@@ -64,4 +64,128 @@ func SubjectIndexFromRemote(image string) (*SubjectIndex, error) {
 		Index: idx,
 		Name:  image,
 	}, nil
+}
+
+const (
+	LocalPrefix               = "oci://"
+	RegistryPrefix            = "docker://"
+	OCI            SourceType = "OCI"
+	Docker         SourceType = "Docker"
+)
+
+type SourceType string
+
+type AttestationOptions struct {
+	NoReferrers   bool
+	Attach        bool
+	ReferrersRepo string
+}
+
+func LoadSubjectIndex(input *ImageSpec) (*SubjectIndex, error) {
+	if input.Type == OCI {
+		return SubjectIndexFromPath(input.WithoutPrefix)
+	} else {
+		return SubjectIndexFromRemote(input.WithoutPrefix)
+	}
+}
+
+func (i *ImageSpec) ForPlatforms(platform string) ([]*ImageSpec, error) {
+	platforms := strings.Split(platform, ",")
+	var specs []*ImageSpec
+	for _, p := range platforms {
+		spec, err := ParseImageSpec(i.OriginalStr, WithPlatform(p))
+		if err != nil {
+			return nil, err
+		}
+		specs = append(specs, spec)
+	}
+	return specs, nil
+}
+
+func ParseImageSpec(img string, options ...ImageSpecOption) (*ImageSpec, error) {
+	img = strings.TrimSpace(img)
+	if strings.Contains(img, ",") {
+		return nil, fmt.Errorf("only one image is supported")
+	}
+	withoutPrefix := strings.TrimPrefix(strings.TrimPrefix(img, LocalPrefix), RegistryPrefix)
+	src := &ImageSpec{
+		OriginalStr:   img,
+		WithoutPrefix: withoutPrefix,
+	}
+	if strings.HasPrefix(img, LocalPrefix) {
+		src.Type = OCI
+	} else {
+		src.Type = Docker
+	}
+	for _, option := range options {
+		err := option(src)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if src.Platform == nil {
+		platform, err := ParsePlatform("")
+		if err != nil {
+			return nil, err
+		}
+		src.Platform = platform
+	}
+	return src, nil
+}
+
+type ImageSpecOption func(*ImageSpec) error
+
+func WithPlatform(platform string) ImageSpecOption {
+	return func(i *ImageSpec) error {
+		if strings.Contains(platform, ",") {
+			return fmt.Errorf("only one platform is supported")
+		}
+		p, err := ParsePlatform(platform)
+		if err != nil {
+			return err
+		}
+		i.Platform = p
+		return nil
+	}
+}
+
+func ParseImageSpecs(img string) ([]*ImageSpec, error) {
+	outputs := strings.Split(img, ",")
+	var sources []*ImageSpec
+	for _, output := range outputs {
+		src, err := ParseImageSpec(output)
+		if err != nil {
+			return nil, err
+		}
+		sources = append(sources, src)
+	}
+	return sources, nil
+}
+
+type ImageSpec struct {
+	// as passed into the constructor
+	OriginalStr string
+	// OCI or Docker
+	Type SourceType
+	// without oci:// or docker://
+	WithoutPrefix string
+	Platform      *v1.Platform
+}
+
+func WithoutTag(image string) (string, error) {
+	notag := image
+	if strings.HasPrefix(image, LocalPrefix) {
+		return image, nil
+	}
+	prefix := ""
+	if strings.HasPrefix(image, RegistryPrefix) {
+		notag = strings.TrimPrefix(image, RegistryPrefix)
+		prefix = RegistryPrefix
+	}
+	ref, err := name.ParseReference(notag)
+	if err != nil {
+		return "", err
+	}
+	repo := ref.Context().Name()
+	return prefix + repo, nil
 }

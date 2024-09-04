54d2d795812c6a1fab41a7e0bba1106197e47e27
James Carnegie <james.carnegie@docker.com>
2024-09-04 10:17:38 +0100

    fix comment

--- policy/policy.go
+++ policy/policy.go
@@ -41,10 +41,7 @@ func CreateAttestationResolver(resolver oci.ImageDetailsResolver, mapping *confi
 	return attestation.NewReferrersResolver(resolver)
 }
 
-// VerifySubject verifies if the given subject PURL matches the image name and platform.
-// e.g. pkg:docker/test-image@test?platform=linux%2Famd64
-// It takes a context.Context, a slice of intoto.Subject, and an attestation.Resolver as parameters.
-// It returns an error if no matching subject is found for the image.
+// VerifySubject verifies if any of the given subject PURLs matches the image name and platform from resolver.
 // Tags are not taken into account when attempting to match because sometimes the user may not have specified a tag, and maybe there
 // isn't a purl subject with that particular tag (because of post build tagging?).
 func VerifySubject(ctx context.Context, subject []intoto.Subject, resolver attestation.Resolver) error {
